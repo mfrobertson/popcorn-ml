ccb970cfa7258afd0409e92f31a9bc92853736fc
mfrobertson <m.robertsonno2@gmail.com>
2025-12-18 16:07:36 +0000

    Update to data grabber

--- src/data/get_data.py
+++ src/data/get_data.py
@@ -1,3 +1,4 @@
+import sys
 from io import BytesIO
 import zipfile
 import requests
@@ -6,27 +7,29 @@ import shutil
 import os
 import sqlite3
 import pandas as pd
-import src
+from src import data_path
 
-DATA_PATH = src.data_path
+DATA_PATH = data_path
 IMDB_EXT = "imdb"
 ML_EXT = "ml"
+SQL_EXT = "sql"
 
-def ml_get(dataset="small"):
-    urls = {
-        "large": "https://files.grouplens.org/datasets/movielens/ml-latest.zip",
-        "small": "https://files.grouplens.org/datasets/movielens/ml-latest-small.zip"
-    }
+def ml_get(small=True):
+    dir_name_orig = "ml-latest-small" if small else "ml-latest"
+    url = f"https://files.grouplens.org/datasets/movielens/{dir_name_orig}.zip"
 
-    ml_dir = os.path.join(DATA_PATH, ML_EXT)
+    ml_dir = DATA_PATH
 
     print("Downloading ml data...")
-    data_get = requests.get(urls[dataset])
+    data_get = requests.get(url)
     data_get.raise_for_status()
 
     data_zip = zipfile.ZipFile(BytesIO(data_get.content))
     data_zip.extractall(ml_dir)
-    print("Completed downloading ml data.")
+
+    dir_name = "ml-small" if small else "ml-large"
+    os.rename(os.path.join(ml_dir, dir_name_orig), os.path.join(ml_dir, dir_name))
+    print(f"Completed downloading {dir_name} data.")
 
 def imdb_get():
     url = "https://datasets.imdbws.com/"
@@ -49,50 +52,67 @@ def imdb_get():
         print("Completed " + name + ".")
     print("Completed imdb download.")
 
-def load_to_sql(ml_type="small"):
+def load_to_sql(name):
 
-    ml_db_path = {"small": "ml-latest-small", "large": "ml-latest"}
-
-    def data_to_sql(dir_path, db, sep=","):
+    def data_to_sql(dir_path, db):
         def get_sep(file_type):
             if file_type == "csv":
                 return ","
             if file_type == "tsv":
                 return "\t"
             raise ValueError("Unsupported file type: " + file_type)
+        def filename_type(file):
+            file_split = file.split(".")
+            file_type = file_split[-1]
+            filename = ".".join(file_split[:-1])
+            return filename, file_type
 
         for file in os.listdir(dir_path):
-            file_name = file[:-4]
-            file_type = file[-3:]
+            filename, file_type = filename_type(file)
             try: sep = get_sep(file_type)
             except: continue
-            print("Processing " + file_name)
+            print("  Processing " + filename)
             df = pd.read_csv(os.path.join(dir_path, file), sep=sep, header=0)
-            df.to_sql(file_name, db, if_exists="replace", index=False, chunksize=10000, method="multi")
+            df.to_sql(filename, db, if_exists="replace", index=False, chunksize=10000, method="multi")
 
-    path_ext = "sql"
-    sql_dir = os.path.join(DATA_PATH, path_ext)
-    ml_dir = os.path.join(DATA_PATH, ML_EXT)
-    imdb_dir = os.path.join(DATA_PATH, IMDB_EXT)
+    sql_dir = os.path.join(DATA_PATH, SQL_EXT)
+    data_dir = os.path.join(DATA_PATH, name)
 
     os.makedirs(sql_dir, exist_ok=True)
 
-    ml_conn = sqlite3.connect(os.path.join(sql_dir, "ml.sqlite"))
-    imdb_conn = sqlite3.connect(os.path.join(sql_dir, "imdb.sqlite"))
+    db_name = name + ".sqlite"
+    sql_conn = sqlite3.connect(os.path.join(sql_dir, db_name))
 
-    print("Loading ml data to sql...")
-    data_to_sql(os.path.join(ml_dir, ml_db_path[ml_type]), ml_conn)
-    print("Completed loading ml data to sql.")
+    print(f"Loading {name} data to sql...")
+    data_to_sql(data_dir, sql_conn)
+    print(f"Completed loading {name} data to sql.")
 
-    print("Loading imdb data to sql...")
-    data_to_sql(imdb_dir, imdb_conn, sep="\t")
-    print("Completed loading imdb data to sql.")
+    sql_conn.close()
 
-    ml_conn.close()
-    imdb_conn.close()
+def remove_dirs(*dirs):
+    for dir in dirs:
+        dir_path = os.path.join(DATA_PATH, dir)
+        print("Removing " + dir_path)
+        if os.path.exists(dir_path):
+            shutil.rmtree(dir_path)
+
+def import_data(*names):
+    for name in names:
+        if name == "imdb":
+            imdb_get()
+        elif name == "ml-small":
+            ml_get()
+        elif name == "ml-large":
+            ml_get(False)
+        else:
+            raise ValueError(f"Unknown data source: {name}")
+        load_to_sql(name)
+
+
+def main(*names):
+    import_data(*names)
+    remove_dirs(*names)
 
 
 if __name__ == "__main__":
-    ml_get("small")
-    imdb_get()
-    load_to_sql()
+    main(*sys.argv[1:])
