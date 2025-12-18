805c685dee9bdb4d82a6c69109c8060cc0908c3d
mfrobertson <m.robertsonno2@gmail.com>
2025-12-18 16:10:20 +0000

    Adding config path in __init__

--- src/__init__.py
+++ src/__init__.py
@@ -3,3 +3,4 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 data_path = os.path.join(dir_path, "../data/")
 keys_path = os.path.join(dir_path, "../_keys/")
+config_path = os.path.join(dir_path, "../_configs/")
