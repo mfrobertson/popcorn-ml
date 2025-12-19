ed2c86ea2eefc05589d126df2d257c29ef43faf6
mfrobertson <m.robertsonno2@gmail.com>
2025-12-19 18:05:05 +0000

    minor

--- src/model/funk_svd.py
+++ src/model/funk_svd.py
@@ -245,15 +245,19 @@ class FunkSVD:
 
         return self
 
-    def predict(self, user_id, item_id) -> float:
+    def predict(self, user_id, item_id, exists=True) -> float:
         """Predict a rating for a user_id, item_id seen during training. If not seen in training, return biased means"""
         if self.user_to_idx_ is None:
             raise RuntimeError("Model not fitted. Call fit() first.")
         if user_id not in self.user_to_idx_:
+            if exists:
+                raise KeyError(f"{user_id} not in user_to_idx_.")
             if item_id not in self.item_to_idx_:
                 return self.mu_
             return self.mu_ + self.bi_[self.item_to_idx_[item_id]]
         if item_id not in self.item_to_idx_:
+            if exists:
+                raise KeyError(f"{item_id} not in item_to_idx_.")
             return self.mu_ + self.bu_[self.user_to_idx_[user_id]]
 
         u = self.user_to_idx_[user_id]
