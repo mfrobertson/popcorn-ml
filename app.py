31b34771f1729b1c1c3f108911062d3ae4aa6720
mfrobertson <m.robertsonno2@gmail.com>
2026-01-18 15:50:11 +0000

    minor

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="Where to Stream", page_icon="🎬")
 
 home = st.Page("pages/page1.py", title="Where to Stream")
-reco = st.Page("pages/page2.py", title="Movie Recommender")
+reco = st.Page("pages/page2.py", title="Movie Recommender", url_path="movie-recommender")
 
 pg = st.navigation([home, reco])
 pg.run()
